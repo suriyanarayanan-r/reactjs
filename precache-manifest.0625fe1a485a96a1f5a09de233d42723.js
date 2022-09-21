97cbc24396e9b828e79324513baeb9f36b1162c8
suriyafromtn <suriyanarayanan.r.2017.it@rajalakshmi.edu.in>
2022-09-21 18:50:15 +0530

    Updates

---
+++ precache-manifest.0625fe1a485a96a1f5a09de233d42723.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1587d41ab3d5674f290",
+    "url": "/reactjs/static/css/main.601ea2dc.chunk.css"
+  },
+  {
+    "revision": "a1587d41ab3d5674f290",
+    "url": "/reactjs/static/js/main.a1587d41.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/reactjs/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "eae2c638fc8a9d976c39",
+    "url": "/reactjs/static/js/2.eae2c638.chunk.js"
+  },
+  {
+    "revision": "a4762d5b0fc5e294c1b9",
+    "url": "/reactjs/static/js/runtime~main.a4762d5b.js"
+  },
+  {
+    "revision": "58d70c0fb7fc7e74255eeb571bc94817",
+    "url": "/reactjs/static/media/suriya water bg final.58d70c0f.jpg"
+  },
+  {
+    "revision": "eddf45f8786dcaaed9be141b08d0da47",
+    "url": "/reactjs/index.html"
+  }
+];
